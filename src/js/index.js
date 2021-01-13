58636731e9c1fd78951572f73dc6035c0da1613a
RomaMirzo <RomaMirzo@users.noreply.github.com>
2021-01-13 23:29:55 +0200

    Add js file

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,7 @@ const app = new PIXI.Application({
   width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
 });
 document.body.appendChild(app.view);
+
+const container = new PIXI.Container();
+
+app.stage.addChild(container);
